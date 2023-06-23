ae0b08a48892888c099210419f5e96a53203acc8
Fet2113 <super.gjoll@yandex.ru>
2023-06-24 02:14:26 +0300

    Домашнее задание - калькулятор сделано. Прошу проверить

--- src/main/java/pro/sky/skyprospringhwcalculator/CalcService.java
+++ src/main/java/pro/sky/skyprospringhwcalculator/CalcService.java
@@ -2,12 +2,8 @@ package pro.sky.skyprospringhwcalculator;
 
 public interface CalcService {
     String calculator();
-
     String plusCalculator(int num1, int num2);
-
     String minusCalculator(int num1, int num2);
-
     String multiplyCalculator(int num1, int num2);
-
     String divideCalculator(int num1, int num2);
 }
--- src/main/java/pro/sky/skyprospringhwcalculator/CalcServiceImpl.java
+++ src/main/java/pro/sky/skyprospringhwcalculator/CalcServiceImpl.java
@@ -11,31 +11,24 @@ public class CalcServiceImpl implements CalcService {
     @Override
     public String plusCalculator(int num1, int num2) {
         int num = num1 + num2;
-
         return num1 + " + " + num2 + " = " + num;
     }
-
     @Override
     public String minusCalculator(int num1, int num2) {
         int num = num1 - num2;
-
         return num1 + " - " + num2 + " = " + num;
     }
-
     @Override
     public String multiplyCalculator(int num1, int num2) {
         int num = num1 * num2;
-
         return num1 + " * " + num2 + " = " + num;
     }
-
     @Override
     public String divideCalculator(int num1, int num2) {
         if (num2 == 0) {
             return " ОШИБКА ";
         } else {
             int num = num1 / num2;
-
             return num1 + " / " + num2 + " = " + num;
         }
     }
